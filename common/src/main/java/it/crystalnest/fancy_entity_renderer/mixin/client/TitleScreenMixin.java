98b9733759d77a532411110873c5794184c2e814
Keksuccino <no@mail.net>
2026-04-05 05:43:38 +0200

    Port to MC 26.1.1

--- common/src/main/java/it/crystalnest/fancy_entity_renderer/mixin/client/TitleScreenMixin.java
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/mixin/client/TitleScreenMixin.java
@@ -16,22 +16,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
+  protected TitleScreenMixin(Component title) {
+    super(title);
+  }
 
-    protected TitleScreenMixin(Component title) {
-        super(title);
+  @Inject(method = "init", at = @At("TAIL"))
+  private void fer$addDevTestButton(CallbackInfo ci) {
+    if (!Services.PLATFORM.isDevEnv() || minecraft == null) {
+      return;
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void fer$addDevTestButton(CallbackInfo ci) {
-        if (!Services.PLATFORM.isDevEnv() || minecraft == null) {
-            return;
-        }
-
-        addRenderableWidget(
-                Button.builder(Component.literal("FER Test"), button -> minecraft.setScreen(new FerDevTestScreen((TitleScreen) (Object) this)))
-                        .bounds(20, 20, 88, 20)
-                        .build()
-        );
-    }
-
+    addRenderableWidget(
+      Button.builder(Component.literal("FER Test"), button -> minecraft.setScreen(new FerDevTestScreen((TitleScreen) (Object) this)))
+        .bounds(20, 20, 88, 20)
+        .build()
+    );
+  }
 }
